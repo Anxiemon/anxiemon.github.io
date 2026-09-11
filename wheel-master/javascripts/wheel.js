9c2b6e7d49dfb2084063af057be435aa3db25988
Anxiemon <114117383+Anxiemon@users.noreply.github.com>
2026-09-11 23:04:19 +0000

    remove more debugging stuff

--- wheel-master/javascripts/wheel.js
+++ wheel-master/javascripts/wheel.js
@@ -320,7 +320,6 @@ var wheel = {
       $('#counter').html((wheel.frames / duration * 1000) + ' FPS');
       var segmentbeingpointedat = wheel.segments.length - Math.floor((wheel.angleCurrent / (Math.PI * 2)) * wheel.segments.length) - 1;
       var selectedStudent = wheel.segments[segmentbeingpointedat];
-      document.getElementById("thingy2").textContent = selectedStudent;
       //^ remove when possible
       handleStudentAction(selectedStudent);
       createStarEffect();
@@ -369,7 +368,6 @@ var wheel = {
       var color = colorsimgonnause[i]
       colorCache.push(color);
     }
-	document.getElementById("thingy").textContent = colorCache;
     wheel.colorCache = colorCache;
 	
     wheel.draw();
